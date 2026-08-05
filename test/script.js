7e26203b8e3cb8e79c2fb45c603bf9bc08ba2e1d
hanchangsu5 <hanchangsu5@gmail.com>
2026-08-05 18:47:49 +0900

    Firestore 저장 테스트 완료

--- test/script.js
+++ test/script.js
@@ -1,7 +1,38 @@
-import { app } from "../firebase.js";
+import { db } from "../firebase.js";
+
+import {
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 const status = document.getElementById("status");
 
 status.textContent = "✅ Firebase 연결 성공!";
 
-console.log("Firebase App :", app);
+async function saveTest() {
+
+    try {
+
+        const docRef = await addDoc(collection(db, "test"), {
+
+            name: "창수",
+            message: "Firebase 테스트 성공",
+            createdAt: new Date()
+
+        });
+
+        console.log("저장 성공 :", docRef.id);
+
+        status.innerHTML += "<br>✅ Firestore 저장 성공!";
+
+    } catch (error) {
+
+        console.error(error);
+
+        status.innerHTML += "<br>❌ Firestore 저장 실패!";
+
+    }
+
+}
+
+saveTest();
