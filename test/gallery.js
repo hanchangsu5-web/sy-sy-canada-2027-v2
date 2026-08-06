2bc4259e93eb30ad517d4c2f0d2bae4e3aec3177
hanchangsu5-web <hanchangsu5@gmail.com>
2026-08-06 13:09:13 +0900

    갤러리 UI 개선 및 모달 기능 추가

--- test/gallery.js
+++ test/gallery.js
@@ -10,6 +10,9 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 const gallery = document.getElementById("gallery");
+let currentDate = "";
+let photoList = [];
+let currentIndex = 0;
 
 function formatDate(date) {
 
@@ -34,34 +37,32 @@ const querySnapshot = await getDocs(q);
 
     querySnapshot.forEach((doc) => {
 
-    const photo = doc.data();
+const photo = doc.data();
 const id = doc.id;
+
+photoList.push(photo);
+
+const photoDate = photo.takenAt
+    ? photo.takenAt.substring(0, 10).replace(/:/g, ".")
+    : "촬영일 없음";
+
+if (photoDate !== currentDate) {
+
+    currentDate = photoDate;
+
+    gallery.innerHTML += `
+        <h2 class="date-title">
+            📅 ${currentDate}
+        </h2>
+    `;
+
+}
 
     gallery.innerHTML += `
         <div class="card">
-            <img src="${photo.photoUrl}">
-            <div class="info">
-
-    <strong>👤 작성자</strong> : ${photo.family}<br>
-
-    <strong>📝 메모</strong> : ${photo.memo}<br>
-
-<strong>📅 촬영</strong> : ${formatDate(photo.takenAt)}<br>
-
-<strong>📍 위치</strong> :
-${
-    photo.latitude
-        ? `<a href="https://www.google.com/maps?q=${photo.latitude},${photo.longitude}" target="_blank">
-            지도 보기
-           </a>`
-        : "-"
-}
-
+    <img src="${photo.photoUrl}" class="photo">
 </div>
 
-<button class="editBtn" data-id="${id}">
-    ✏️ 메모 수정
-</button>
         </div>
     `;
 
@@ -72,6 +73,22 @@ ${
 loadPhotos();
 
 gallery.addEventListener("click", async (event) => {
+
+    if (event.target.classList.contains("photo")) {
+
+    const photos = document.querySelectorAll(".photo");
+
+    currentIndex = Array.from(photos).indexOf(event.target);
+
+    document.getElementById("photoModal").style.display = "flex";
+
+    document.getElementById("modalImage").src = event.target.src;
+
+    console.log("현재 사진 :", currentIndex);
+
+    return;
+
+}
 
     if (!event.target.classList.contains("editBtn")) {
 
@@ -98,5 +115,44 @@ await updateDoc(doc(db, "photos", id), {
 alert("메모가 수정되었습니다.");
 
 location.reload();
+
+});
+
+const modal = document.getElementById("photoModal");
+const closeModal = document.getElementById("closeModal");
+
+closeModal.addEventListener("click", () => {
+
+    modal.style.display = "none";
+
+});
+
+let startY = 0;
+
+const modalImage = document.getElementById("modalImage");
+
+modalImage.addEventListener("touchstart", (event) => {
+
+    startY = event.touches[0].clientY;
+
+});
+
+modalImage.addEventListener("touchend", (event) => {
+
+    const endY = event.changedTouches[0].clientY;
+
+    const distance = startY - endY;
+
+    if (distance > 50) {
+
+        alert("⬆ 다음 사진");
+
+    }
+
+    else if (distance < -50) {
+
+        alert("⬇ 이전 사진");
+
+    }
 
 });
